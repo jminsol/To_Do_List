5982535b361d50783548417c336acb4add1e10dc
Minsol Jeong <jminsol0525@gmail.com>
2021-03-07 00:55:48 -0800

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Minsol Jeong on 2021/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
